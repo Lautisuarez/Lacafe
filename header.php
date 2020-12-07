4e636d0242b5c8fa6264f2cc00021c1ec0009633
Lautisuarez <53661725+Lautisuarez@users.noreply.github.com>
2020-12-06 21:49:12 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lacáfe</title>
+    
+    <!-- FONT AWESOME -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <!-- ICONO -->
+    <link rel="manifest" href="site.webmanifest">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <!-- NORMALIZE -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body class="footer-fijo">
+    <!-- HEADER -->
+    <header class="site-header">
+        <div class="contenedor header">
+            <a href="index.html"><img src="img/logo.jpg" alt="Logotipo"></a>
+            <nav class="nav">
+                <a href="index.php">Inicio</a>
+            </nav>
+        </div>
+    </header>

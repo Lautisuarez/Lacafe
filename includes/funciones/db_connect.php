4a6c64e54ac62cf2ad240c7da875809d881fe0f1
lautaro <lauti_campeon7@hotmail.com>
2021-01-13 11:24:21 -0300

    Arreglando error a conexion base de datos

--- includes/funciones/db_connect.php
+++ includes/funciones/db_connect.php
@@ -1,7 +1,12 @@
 <?php 
-    $conn = new mysqli('localhost', 'root', 'root', 'lacafe');
+    define('GDLWEBCAMP_HOST', 'badl3ucmudvmmtz46xmk-mysql.services.clever-cloud.com');
+    define('GDLWEBCAMP_DB_USUARIO', 'uruf31onibgt4hrq');
+    define('GDLWEBCAMP_DB_PASSWORD', 'vAro82r8VKRfz68Fd4Vf');
+    define('GDLWEBCAMP_DB_DATABASE', 'badl3ucmudvmmtz46xmk');
 
-    if($conn->connect_error){
-        echo $error -> $conn->connect_error;
+    $conn = new mysqli(GDLWEBCAMP_HOST, GDLWEBCAMP_DB_USUARIO, GDLWEBCAMP_DB_PASSWORD, GDLWEBCAMP_DB_DATABASE);
+
+    if($conn->connect_error) {
+      echo $conn->connect_error;
     }
 ?>
